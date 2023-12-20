23aec4fd57407c0cdec36baeb22bdc55bc4d2d7a
Robotmcgregor <35555135+Robotmcgregor@users.noreply.github.com>
2023-12-20 11:39:29 +0930

    base html added

--- todolist_app/urls.py
+++ todolist_app/urls.py
@@ -3,7 +3,9 @@ from todolist_app import views
 
 
 urlpatterns = [
-    path('task/', views.todolist, name='todolist'),
+    path('', views.todolist, name='todolist'),
+    path('contact/', views.contact, name='contact'),
+    path('about-us/', views.about, name='about'),
     # path('task/delete/<task_id>', views.delete_task, name='delete_task'),
     # path('task/edit/<task_id>', views.edit_task, name='edit_task'),
     # path('task/complete/<task_id>', views.complete_task, name='complete_task'),
--- todolist_app/views.py
+++ todolist_app/views.py
@@ -4,6 +4,20 @@ from django.http import HttpResponse
 def todolist(request):
     #return HttpResponse("Welcome")
     context = {
-        'welcome_text':'Welcome To Todo List App!',
+        'todolist_text':'Welcome To Todo List App!',
                }
     return render(request, 'todolist.html', context)
+
+def contact(request):
+    #return HttpResponse("Welcome")
+    context = {
+        'contact_text':'Welcome To Contact Us!',
+               }
+    return render(request, 'contact.html', context)
+
+def about(request):
+    #return HttpResponse("Welcome")
+    context = {
+        'about_text':'Welcome To About Us!',
+               }
+    return render(request, 'about.html', context)
