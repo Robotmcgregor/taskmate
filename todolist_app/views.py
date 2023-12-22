7bd0a7851a0150aaa97c26ab547d282b1daa36aa
Robotmcgregor <35555135+Robotmcgregor@users.noreply.github.com>
2023-12-22 11:52:35 +0930

    accept inputs

--- todolist_app/views.py
+++ todolist_app/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from todolist_app.models import TaskList
+from todolist_app.forms import TaskForm
 
 def todolist(request):
-    #return HttpResponse("Welcome")
+    if request.method == "POST":
+        form = TaskForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        return redirect('todolist')
+    
+    else:
+        all_tasks = TaskList.objects.all()
+        return render(request, 'todolist.html', {'all_tasks': all_tasks})
+    
     context = {
         'todolist_text':'Welcome To Todo List App!',
+        'all_tasks': all_tasks,
                }
+    
     return render(request, 'todolist.html', context)
+
+
 
 def contact(request):
     #return HttpResponse("Welcome")
