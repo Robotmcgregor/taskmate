ea7f1b4264c7625b74de1d5f60cc1eb90bcd7beb
Robotmcgregor <35555135+Robotmcgregor@users.noreply.github.com>
2023-12-27 09:01:37 +0930

    new task message added.

--- todolist_app/forms.py
+++ todolist_app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from todolist_app.models import TaskList
 
+# model indicates which database you are connecting to
 class TaskForm(forms.ModelForm):
     class Meta:
         model = TaskList
--- todolist_app/views.py
+++ todolist_app/views.py
@@ -2,24 +2,20 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from todolist_app.models import TaskList
 from todolist_app.forms import TaskForm
+from django.contrib import messages
 
 def todolist(request):
     if request.method == "POST":
         form = TaskForm(request.POST or None)
         if form.is_valid():
             form.save()
+        messages.success(request, ("New Task Added!"))
         return redirect('todolist')
     
     else:
         all_tasks = TaskList.objects.all()
         return render(request, 'todolist.html', {'all_tasks': all_tasks})
     
-    context = {
-        'todolist_text':'Welcome To Todo List App!',
-        'all_tasks': all_tasks,
-               }
-    
-    return render(request, 'todolist.html', context)
 
 
 
