f73d1017654a2dde2547579cf8eeaaffb35dd735
Robotmcgregor <35555135+Robotmcgregor@users.noreply.github.com>
2023-12-27 10:26:50 +0930

    editing task completed for app

--- todolist_app/urls.py
+++ todolist_app/urls.py
@@ -5,7 +5,8 @@ from todolist_app import views
 urlpatterns = [
     # path('', views.index, name='index'),
     path('todolist', views.todolist, name='todolist'),
-    path('delete/<task_id>', views.delete_task, name='delete_task'),
+    path('todolist/delete/<task_id>', views.delete_task, name='delete_task'),
+    path('todolist/edit/<task_id>', views.edit_task, name='edit_task'),
         
     path('contact/', views.contact, name='contact'),
     path('about-us/', views.about, name='about'),
--- todolist_app/views.py
+++ todolist_app/views.py
@@ -16,7 +16,29 @@ def todolist(request):
         all_tasks = TaskList.objects.all()
         return render(request, 'todolist.html', {'all_tasks': all_tasks})
     
+    
+def delete_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.delete()
+    
+    return redirect('todolist')
 
+
+def edit_task(request, task_id):
+    if request.method == "POST":
+        task = TaskList.objects.get(pk=task_id)
+        # instance is used to tell django which object to update
+        form = TaskForm(request.POST or None, instance=task)
+        if form.is_valid():
+            form.save()
+        
+        
+        messages.success(request, ("Task Edited!"))
+        return redirect('todolist')
+    
+    else:
+        task_obj = TaskList.objects.get(pk=task_id)
+        return render(request, 'edit.html', {'task_obj': task_obj})
 
 
 def contact(request):
