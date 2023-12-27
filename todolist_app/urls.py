dcae08bd561fceb88281843707c01940864f60d4
Robotmcgregor <35555135+Robotmcgregor@users.noreply.github.com>
2023-12-27 09:50:07 +0930

    delete url added to app url page

--- todolist_app/urls.py
+++ todolist_app/urls.py
@@ -5,6 +5,8 @@ from todolist_app import views
 urlpatterns = [
     # path('', views.index, name='index'),
     path('todolist', views.todolist, name='todolist'),
+    path('delete/<task_id>', views.delete_task, name='delete_task'),
+        
     path('contact/', views.contact, name='contact'),
     path('about-us/', views.about, name='about'),
     # path('task/delete/<task_id>', views.delete_task, name='delete_task'),
