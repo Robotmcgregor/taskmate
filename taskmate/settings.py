8c3a0911f5fbe19d34fbdbfec4b57e35660c70e4
Robotmcgregor <35555135+Robotmcgregor@users.noreply.github.com>
2023-12-30 08:22:04 +0930

    postgresql working

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -80,10 +80,21 @@ WSGI_APPLICATION = 'taskmate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Boris@1978',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
