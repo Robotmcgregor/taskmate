64924cf751fc36d4d3cf844efea4bb940ee7269e
Robotmcgregor <35555135+Robotmcgregor@users.noreply.github.com>
2023-12-29 11:36:18 +0930

    login logout on nav page

--- taskmate/urls.py
+++ taskmate/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', todolist_views.index, name='index'),
     path('todolist/', include('todolist_app.urls')),
-    path('contact', todolist_views.contact, name='contact'),
-    path('about', todolist_views.about, name='about'),
+    path('contact/', todolist_views.contact, name='contact'),
+    path('about/', todolist_views.about, name='about'),
     
     path('account/', include('users_app.urls')),
 
