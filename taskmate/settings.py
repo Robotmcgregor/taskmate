7f4eeaad93c407b4be8bbcb22b0e460849c8891d
Robotmcgregor <35555135+Robotmcgregor@users.noreply.github.com>
2023-12-30 09:59:41 +0930

    git init

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+# reading .env file
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=6u-&ago0+9s#w1b0g=1016l5pl(+@36c-*srqbbbm0&^bq3qs'
+#SECRET_KEY = 'django-insecure-=6u-&ago0+9s#w1b0g=1016l5pl(+@36c-*srqbbbm0&^bq3qs'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DJANGO_DEBUG')
 
 # domains where the project will function
 ALLOWED_HOSTS = []
@@ -90,11 +96,11 @@ WSGI_APPLICATION = 'taskmate.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'Boris@1978',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': env('POSTGRESQL_NAME'),
+        'USER': env('POSTGRESQL_USER'),
+        'PASSWORD': env('POSTGRESQL_PASSWORD'),
+        'HOST': env('POSTGRESQL_HOST'),
+        'PORT': env('POSTGRESQL_PORT'),
     }
 }
 
